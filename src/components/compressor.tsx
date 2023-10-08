27dd939347c42c0fce9a0d0dff41722f3146c1eb
Andrew Shearer <andrewshearerdev@gmail.com>
2023-10-08 13:49:31 -0700

    installed correct lucide pckg, and updated drop-area jsx

--- src/components/compressor.tsx
+++ src/components/compressor.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-import { useState } from "react";
-
 import { DropArea } from "@/components/drop-area";
 import { Results } from "@/components/results";
 
 const Compressor = () => {
-  // state
-  const [counter, setCounter] = useState(1);
-
   return (
     <>
       <DropArea />
--- src/components/drop-area.tsx
+++ src/components/drop-area.tsx
@@ -1,5 +1,13 @@
+import { Download } from "lucide-react";
+
 const DropArea = () => {
-  return <div>DropArea</div>;
+  return (
+    <section className="droparea">
+      <Download size={55} />
+      <p>Drop your .png or .jpg files here!</p>
+      <small>Up to 20 images, max 4 MB each.</small>
+    </section>
+  );
 };
 
 export { DropArea };
