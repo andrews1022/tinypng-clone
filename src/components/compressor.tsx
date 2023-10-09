0ab829eed41fe90dbae7fc7a914ad0b8760b0cb2
Andrew Shearer <andrewshearerdev@gmail.com>
2023-10-09 00:14:14 -0700

    implemented total percent and file size saved functionality

--- src/components/compressor.tsx
+++ src/components/compressor.tsx
@@ -167,6 +167,21 @@ const Compressor = () => {
     });
   };
 
+  const totalPercentSaved = results
+    .reduce((total, result) => {
+      return total + parseFloat(result.percentSaved);
+    }, 0)
+    .toFixed(2);
+  // In this code, totalPercentSaved is calculated by using the reduce function on the results array. It adds up the individual percentSaved values for each image and formats the result to have 2 decimal places.
+
+  const totalSizeSaved = results.reduce((total, result) => {
+    const originalSize = result.originalFile.size;
+    const newSize = result.newFile.size;
+    const sizeDifference = originalSize - newSize;
+    return total + sizeDifference;
+  }, 0);
+  // In this code, totalSizeSaved is calculated by using the reduce function similarly to the total percent saved. It adds up the differences in file sizes (in bytes) for each image.
+
   return (
     <>
       <section
@@ -229,6 +244,15 @@ const Compressor = () => {
           <button className="results__download-all" onClick={handleDownloadAll}>
             <DownloadCloud /> Download All
           </button>
+        </section>
+      ) : null}
+
+      {allImagesDoneCompressing && results.length ? (
+        <section className="totals">
+          <p className="totals__message">
+            We just saved you <span className="totals__percent">{totalPercentSaved}%</span>
+            <span className="totals__size-saved">{getFileSizeString(totalSizeSaved)} total</span>
+          </p>
         </section>
       ) : null}
     </>
