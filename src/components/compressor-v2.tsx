4a6ca1417e809677450050232998d143d5242f68
Andrew Shearer <andrewshearerdev@gmail.com>
2023-10-10 10:35:06 -0700

    continued working on compressor-v2
    - swapped out section for form and input to use more semantic elements
    - created reusable functio to handle various drop events

--- src/components/compressor-v2.tsx
+++ src/components/compressor-v2.tsx
@@ -16,8 +16,84 @@ declare global {
   }
 }
 
+// constants
+const MAX_NUMBER_OF_FILES_UPLOADED_AT_ONCE = 20;
+const MAX_FILE_SIZE = 4 * 1024 * 1024; // 4 MB
+
 const CompressorV2 = () => {
-  return <div>CompressorV2</div>;
+  // state
+  const [dropAreaInUse, setDropAreaInUse] = useState(false);
+  const [imagesToCompress, setImagesToCompress] = useState([]);
+
+  const handleDrop = (event: DragEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const { files } = event.dataTransfer;
+
+    console.log(files);
+
+    if (files.length > MAX_NUMBER_OF_FILES_UPLOADED_AT_ONCE) {
+      setDropAreaInUse(false);
+
+      return alert("Too many files!");
+    }
+
+    // handleFiles(images);
+
+    setDropAreaInUse(false);
+  };
+
+  const handleDragEvents = (event: DragEvent<HTMLFormElement>, dropAreaInUse: boolean) => {
+    event.preventDefault();
+    setDropAreaInUse(dropAreaInUse);
+  };
+
+  // const handleDragEnter = (event: DragEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   setDropAreaInUse(true);
+  // };
+
+  // const handleDragOver = (event: DragEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   setDropAreaInUse(true);
+  // };
+
+  // const handleDragLeave = (event: DragEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   setDropAreaInUse(false);
+  // };
+
+  const handleClick = () => {
+    console.log("clicked!");
+  };
+
+  return (
+    <>
+      <form
+        className={`droparea ${dropAreaInUse ? "droparea--in-use" : ""}`}
+        onDrop={handleDrop}
+        onDragEnter={(event) => handleDragEvents(event, true)}
+        onDragOver={(event) => handleDragEvents(event, true)}
+        onDragLeave={(event) => handleDragEvents(event, false)}
+      >
+        <Download size={55} />
+        <label htmlFor="select-images">
+          Drag and drop or <span className="text-amber-500">click here to browse</span>
+        </label>
+        <input
+          type="file"
+          id="select-images"
+          name="select-images"
+          accept="image/*"
+          multiple
+          // onClick={handleClick}
+          className="hidden"
+          // onChange={handleFiles}
+        />
+        <small>Up to 20 images, max 4 MB each.</small>
+      </form>
+    </>
+  );
 };
 
 export { CompressorV2 };
