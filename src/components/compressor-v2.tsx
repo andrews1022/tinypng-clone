e0a47db5bce1900cd811aacf15aec71da86e68e7
Andrew Shearer <andrewshearerdev@gmail.com>
2023-10-10 10:58:35 -0700

    updated handleChange & handleDrop to prepare files for handleFiles fn

--- src/components/compressor-v2.tsx
+++ src/components/compressor-v2.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 
 import { compressFile, getFileSizeString } from "@/lib/utils";
 
-import type { DragEvent } from "react";
+import type { ChangeEvent, DragEvent } from "react";
 import type { Result } from "@/types";
 
 declare global {
@@ -16,6 +16,14 @@ declare global {
   }
 }
 
+type FileReadyForCompressing = {
+  file: File;
+  large: boolean;
+  name: string;
+  size: number;
+  type: string;
+};
+
 // constants
 const MAX_NUMBER_OF_FILES_UPLOADED_AT_ONCE = 20;
 const MAX_FILE_SIZE = 4 * 1024 * 1024; // 4 MB
@@ -23,48 +31,76 @@ const MAX_FILE_SIZE = 4 * 1024 * 1024; // 4 MB
 const CompressorV2 = () => {
   // state
   const [dropAreaInUse, setDropAreaInUse] = useState(false);
-  const [imagesToCompress, setImagesToCompress] = useState([]);
+  const [imagesReadyToCompress, setImagesReadyToCompress] = useState<FileReadyForCompressing[]>([]);
+
+  const handleFiles = (files: FileReadyForCompressing[]) => {
+    console.log("files --> ", files);
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+    // console.log(files);
+
+    if (files) {
+      if (files.length > MAX_NUMBER_OF_FILES_UPLOADED_AT_ONCE) {
+        setDropAreaInUse(false);
+        return alert("Too many files! Please upload no more than 20 files at once.");
+      }
+
+      // prepare files for compression
+      const preppedFiles: FileReadyForCompressing[] = Array.from(files).map((file) => {
+        const { name, size, type } = file;
+
+        return {
+          file,
+          large: size > MAX_FILE_SIZE,
+          name,
+          size,
+          type
+        };
+      });
+
+      // setImagesReadyToCompress(preppedFiles);
+
+      setDropAreaInUse(false);
+      handleFiles(preppedFiles);
+    }
+  };
 
   const handleDrop = (event: DragEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const { files } = event.dataTransfer;
-
-    console.log(files);
+    // console.log(files);
 
     if (files.length > MAX_NUMBER_OF_FILES_UPLOADED_AT_ONCE) {
       setDropAreaInUse(false);
-
-      return alert("Too many files!");
+      return alert("Too many files! Please upload no more than 20 files at once.");
     }
 
-    // handleFiles(images);
+    // prepare files for compression
+    const preppedFiles: FileReadyForCompressing[] = Array.from(files).map((file) => {
+      const { name, size, type } = file;
+
+      return {
+        file,
+        large: size > MAX_FILE_SIZE,
+        name,
+        size,
+        type
+      };
+    });
+
+    // setImagesReadyToCompress(preppedFiles);
 
     setDropAreaInUse(false);
+    handleFiles(preppedFiles);
   };
 
+  // reusable function to handle drag events
   const handleDragEvents = (event: DragEvent<HTMLFormElement>, dropAreaInUse: boolean) => {
     event.preventDefault();
     setDropAreaInUse(dropAreaInUse);
-  };
-
-  // const handleDragEnter = (event: DragEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-  //   setDropAreaInUse(true);
-  // };
-
-  // const handleDragOver = (event: DragEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-  //   setDropAreaInUse(true);
-  // };
-
-  // const handleDragLeave = (event: DragEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-  //   setDropAreaInUse(false);
-  // };
-
-  const handleClick = () => {
-    console.log("clicked!");
   };
 
   return (
@@ -77,18 +113,20 @@ const CompressorV2 = () => {
         onDragLeave={(event) => handleDragEvents(event, false)}
       >
         <Download size={55} />
+
         <label htmlFor="select-images">
-          Drag and drop or <span className="text-amber-500">click here to browse</span>
+          Drag and drop or{" "}
+          <span className="text-amber-500 font-semibold">click here to browse</span>
         </label>
+
         <input
           type="file"
           id="select-images"
           name="select-images"
           accept="image/*"
           multiple
-          // onClick={handleClick}
           className="hidden"
-          // onChange={handleFiles}
+          onChange={handleChange}
         />
         <small>Up to 20 images, max 4 MB each.</small>
       </form>
